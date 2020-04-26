b05c8fbb4c2608e20ef3290901a40fb9b855f46c
Piotr Stanny <piotr.stanny@gmail.com>
2020-04-26 20:06:43 +0100

    Quote component created

--- src/App.vue
+++ src/App.vue
@@ -2,18 +2,26 @@
     <div class="container">
         <div class="row">
             <div class="col-xs-12">
-               <h1>Hello world!</h1>
+				<h1>Hello world!</h1>
+				<quote/>
             </div>
         </div>
     </div>
 </template>
 
 <script>
+    import Quote from './components/Quote.vue'
 
     export default {
-        
+		components: {
+			Quote
+		}
     }
 </script>
 
-<style>
+<style scoped>
+div {
+	margin: 0 auto;
+	text-align: center;
+}
 </style>
---
+++ src/components/Quote.vue
@@ -0,0 +1,22 @@
+<template>
+	<div>
+		<p><q>To envision the future, one must learn from the past to create a new tomorrow.</q></p>
+	</div>
+</template>
+
+<script>
+export default {
+	
+}
+</script>
+
+<style scoped>
+div {
+	margin: 40px auto;
+	padding: 30px;
+	box-shadow: 10px 10px 15px rgb(53, 53, 53);
+	border: 1px solid rgb(206, 206, 206);
+	text-align: center;
+
+}
+</style>
